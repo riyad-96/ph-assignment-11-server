4d630505c6171be389f202773a5eb62bbe9fba19
riyad-96 <riyadofficial.ide@gmail.com>
2025-12-10 00:27:33 +0600

    fraud status update routed added

---
+++ src/controllers/admin/updateIsFraud.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from 'express';
+import { usersCollection } from '../../connections/mongodb.connection.js';
+import { ObjectId } from 'mongodb';
+
+export default async function updateIsFraud(req: Request, res: Response) {
+  try {
+    const { id, isFraud } = req.body;
+    const userColl = usersCollection();
+    await userColl.findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          isFraud,
+        },
+      },
+    );
+    res.send({code: 'FRAUD_STATUS_UPDATED', message: 'Fraud status was updated'});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ code: 'INTERNAL_SERVER_ERROR', message: 'Error while fetching data' });
+  }
+}
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,10 +4,12 @@ import express from 'express';
 // Local imports
 import getUsersData from '../controllers/admin/getUsersData.js'
 import updateRole from '../controllers/admin/updateRole.js'
+import updateIsFraud from '../controllers/admin/updateIsFraud.js';
 
 const router = express.Router();
 
 router.get('/get-user-data', getUsersData);
-router.post('/update-role', updateRole)
+router.post('/update-role', updateRole);
+router.post('/update-is-fraud', updateIsFraud)
 
 export { router as adminRouter };
